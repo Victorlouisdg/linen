7ec6809c268efa9f5f2e50ccfd80a376a9c3b417
victorlouisdg <victorlouisdg@gmail.com>
2023-04-18 11:47:40 +0200

    finish annotation example

--- linen/linen/opencv/annotation.py
+++ linen/linen/opencv/annotation.py
@@ -28,7 +28,7 @@ def draw_infinite_line(
     image: np.ndarray,
     point0: Tuple[int, int],
     point1: Tuple[int, int],
-    color=(255, 255, 0),
+    color: Tuple[int, int, int] = (255, 255, 0),
 ) -> np.ndarray:
     line_start = np.array(point0)
     line_end = np.array(point1)
@@ -41,7 +41,106 @@ def draw_infinite_line(
     line_end = line_end + line_direction * 10000
     line_start = tuple(line_start.astype(int))
     line_end = tuple(line_end.astype(int))
-    image = cv2.line(image, line_start, line_end, color, 1)
+    image = cv2.arrowedLine(image, line_start, line_end, color, 1)  # Arrow is currently off screen
+    return image
+
+
+def draw_keypoint_annotation(
+    image: np.ndarray,
+    point: Tuple[int, int],
+    name: str,
+    color: Tuple[int, int, int] = (0, 255, 0),
+) -> np.ndarray:
+    text_location = (point[0] + 8, point[1])
+    image = cv2.circle(image, point, 3, color, -1)
+    image = cv2.putText(
+        image,
+        name,
+        text_location,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        (255, 255, 0),
+        1,
+    )
+    return image
+
+
+def draw_line_segment_annotation(
+    image: np.ndarray,
+    point0: Tuple[int, int],
+    point1: Tuple[int, int],
+    name: str,
+    color: Tuple[int, int, int] = (0, 255, 0),
+) -> np.ndarray:
+    text_location_start = (point0[0] + 8, point0[1])
+    image = cv2.circle(image, point0, 3, color, -1)
+    image = cv2.circle(image, point1, 3, color, -1)
+    image = cv2.line(image, point0, point1, color, 1)
+    image = cv2.putText(
+        image,
+        f"{name}_start",
+        text_location_start,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        color,
+        1,
+    )
+    text_location_end = (point1[0] + 8, point1[1])
+    image = cv2.putText(
+        image,
+        f"{name}_end",
+        text_location_end,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        color,
+        1,
+    )
+    return image
+
+
+def draw_line_annotation(
+    image: np.ndarray,
+    point0: Tuple[int, int],
+    point1: Tuple[int, int],
+    name: str,
+    color: Tuple[int, int, int] = (0, 255, 0),
+) -> np.ndarray:
+    text_location = (point0[0] + 8, point0[1])
+    image = cv2.circle(image, point0, 3, color, -1)
+    image = cv2.circle(image, point1, 3, color, -1)
+    image = draw_infinite_line(image, point0, point1, color)
+    image = cv2.putText(
+        image,
+        name,
+        text_location,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        color,
+        1,
+    )
+    return image
+
+
+def draw_bounding_box_annotation(
+    image: np.ndarray,
+    point0: Tuple[int, int],
+    point1: Tuple[int, int],
+    name: str,
+    color: Tuple[int, int, int] = (0, 255, 0),
+) -> np.ndarray:
+    text_location = (point0[0] + 8, point0[1])
+    image = cv2.circle(image, point0, 3, color, -1)
+    image = cv2.circle(image, point1, 3, color, -1)
+    image = cv2.rectangle(image, point0, point1, color, 1)
+    image = cv2.putText(
+        image,
+        name,
+        text_location,
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.5,
+        color,
+        1,
+    )
     return image
 
 
@@ -69,67 +168,18 @@ def process_clicked_points(annotation_type: Annotation, clicked_points: list) ->
 
 def visualize_finished_annotations(image: np.ndarray, annotations: dict, annotation_spec) -> np.ndarray:
     """Visualize the finished annotations on the image."""
+    cyan = (255, 255, 0)
 
     for name, annotation in annotations.items():
         annotation_type = annotation_spec[name]
-
         if annotation_type == Annotation.Keypoint:
-            point = annotation
-            text_location = (annotation[0] + 8, annotation[1])
-            image = cv2.circle(image, point, 3, (255, 255, 0), -1)
-            image = cv2.putText(
-                image,
-                name,
-                text_location,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 0),
-                1,
-            )
+            draw_keypoint_annotation(image, annotation, name, cyan)
         if annotation_type == Annotation.LineSegment:
-            point1, point2 = annotation
-            text_location = (annotation[0][0] + 8, annotation[0][1])
-            image = cv2.circle(image, point1, 3, (255, 255, 0), -1)
-            image = cv2.circle(image, point2, 3, (255, 255, 0), -1)
-            image = cv2.line(image, point1, point2, (255, 255, 0), 1)
-            image = cv2.putText(
-                image,
-                name,
-                text_location,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 0),
-                1,
-            )
-
+            draw_line_segment_annotation(image, annotation[0], annotation[1], name, cyan)
         if annotation_type == Annotation.Line:
-            # only draw first point and then the line that extend over the full image
-            point0, point1 = annotation
-            image = draw_infinite_line(image, point0, point1)
-            image = cv2.circle(image, point0, 3, (255, 255, 0), -1)
-            image = cv2.putText(
-                image,
-                name,
-                text_location,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 0),
-                1,
-            )
-
+            draw_line_annotation(image, annotation[0], annotation[1], name, cyan)
         if annotation_type == Annotation.BoundingBox:
-            point1, point2 = annotation
-            text_location = (annotation[0][0] + 8, annotation[0][1])
-            image = cv2.rectangle(image, point1, point2, (255, 255, 0), 1)
-            image = cv2.putText(
-                image,
-                name,
-                text_location,
-                cv2.FONT_HERSHEY_SIMPLEX,
-                0.5,
-                (255, 255, 0),
-                1,
-            )
+            draw_bounding_box_annotation(image, annotation[0], annotation[1], name, cyan)
 
     return image
 
@@ -144,42 +194,31 @@ def visualize_current_annotation(
     """Visualize the current annotation on the image."""
 
     if annotation_type == Annotation.Keypoint:
-        text = f"Click to annotate keypoint: {annotation_name}"
+        banner_text = f"Click to annotate keypoint: {annotation_name}"
 
     if annotation_type == Annotation.LineSegment:
-        text = f"Click to annotate line segment: {annotation_name}"
+        banner_text = f"Click to annotate line segment: {annotation_name}"
 
         if len(clicked_points) == 1:
-            point = clicked_points[0]
-            image = cv2.circle(image, point, 3, (0, 255, 0), -1)
-            # draw a line between the point and the current mouse position
-            image = cv2.line(image, point, current_mouse_point, (0, 255, 0), 1)
+            draw_line_segment_annotation(image, clicked_points[0], current_mouse_point, annotation_name)
 
     if annotation_type == Annotation.Line:
-        text = f"Click to annotate line: {annotation_name}"
+        banner_text = f"Click to annotate line: {annotation_name}"
 
         if len(clicked_points) == 1:
-            point0 = clicked_points[0]
-            point1 = current_mouse_point
-            print(point0)
-            print(point1)
-            image = cv2.circle(image, point0, 3, (0, 255, 0), -1)
-            image = draw_infinite_line(image, point0, point1, color=(0, 255, 0))
+            draw_line_annotation(image, clicked_points[0], current_mouse_point, annotation_name)
 
     if annotation_type == Annotation.BoundingBox:
-        text = f"Click to annotate bounding box: {annotation_name}"
+        banner_text = f"Click to annotate bounding box: {annotation_name}"
 
         if len(clicked_points) == 1:
-            point1 = clicked_points[0]
-            point2 = current_mouse_point
-            image = cv2.rectangle(image, point1, point2, (0, 255, 0), 1)
+            draw_bounding_box_annotation(image, clicked_points[0], current_mouse_point, annotation_name)
 
-    cv2.putText(image, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
+    cv2.putText(image, banner_text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     return image
 
 
-def get_manual_annotations(camera: Camera, annotation_spec: dict) -> dict:
+def get_manual_annotations(camera: Camera, annotation_spec: dict[str, Annotation]) -> dict:
     current_mouse_point = [(0, 0)]  # has to be a list so that the callback can edit it
     clicked_points = []
 
@@ -200,27 +239,40 @@ def get_manual_annotations(camera: Camera, annotation_spec: dict) -> dict:
     current_id = 0
 
     while True:
-        _, h, w = camera.get_rgb_image().shape
         image = camera.get_rgb_image()
-
         image = ImageConverter(image).image_in_opencv_format
 
-        print(len(clicked_points))
+        if current_id >= num_annotations:
+            banner_text = "All annotations collected. Press 'Enter' to confirm."
+            cv2.putText(
+                image,
+                banner_text,
+                (10, 30),
+                cv2.FONT_HERSHEY_SIMPLEX,
+                1,
+                (0, 255, 0),
+                2,
+            )
+        else:
+            # Process and draw
+            current_name = annotation_names[current_id]
+            current_type = annotation_types[current_id]
 
-        # Process and draw
-        current_name = annotation_names[current_id]
-        current_type = annotation_types[current_id]
+            clicked_points, annotation = process_clicked_points(current_type, clicked_points)
 
-        clicked_points, annotation = process_clicked_points(current_type, clicked_points)
+            if annotation is not None:
+                annotations[current_name] = annotation
+                current_id += 1
 
-        if annotation is not None:
-            annotations[current_name] = annotation
-            current_id += 1
-            if current_id >= num_annotations:
-                return annotations
+            image = visualize_current_annotation(
+                image,
+                current_name,
+                current_type,
+                current_mouse_point[0],
+                clicked_points,
+            )
 
         image = visualize_finished_annotations(image, annotations, annotation_spec)
-        image = visualize_current_annotation(image, current_name, current_type, current_mouse_point[0], clicked_points)
 
         cv2.imshow(window_name, image)
         key = cv2.waitKey(10)
@@ -228,3 +280,26 @@ def get_manual_annotations(camera: Camera, annotation_spec: dict) -> dict:
         if key == ord("q"):
             cv2.destroyAllWindows()
             return None
+
+        # Enter key
+        if key == 13 and current_id >= num_annotations:
+            cv2.destroyAllWindows()
+            return annotations
+
+
+if __name__ == "__main__":
+    import pprint
+
+    from airo_camera_toolkit.cameras.zed2i import Zed2i
+
+    camera = Zed2i(resolution=Zed2i.RESOLUTION_720, fps=30)
+    annotation_spec = {
+        "keypoint": Annotation.Keypoint,
+        "line_segment": Annotation.LineSegment,
+        "line": Annotation.Line,
+        "bounding_box": Annotation.BoundingBox,
+    }
+    annotations = get_manual_annotations(camera, annotation_spec)
+
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(annotations)
